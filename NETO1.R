87fa864cef3b78ffe1439832a5aaca60f176735d
AiridaLi <47628880+AiridaLi@users.noreply.github.com>
2019-05-08 18:50:58 +0300

    Update NETO1.R

--- NETO1.R
+++ NETO1.R
@@ -6,8 +6,6 @@ bruto_neto<-function(x){
         }else if(x>=2555){
                 NPD<-0
         }else{NPD<-300}
-        
-        
         VDU<-1283.2
         if (x>=VDU*10){
                 GPM<-(10*VDU*0.2+(x-10*VDU)*0.27) 
@@ -15,7 +13,6 @@ bruto_neto<-function(x){
                  x>300){
                 GPM<-(x-NPD)*0.2
         }else{GPM<-0}
-        
         PSD<-x*0.0698
         if(x<10*VDU){
                 SOC<-x*0.1252
